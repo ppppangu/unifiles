7ef688bf529696e7a01ca70c85b0c5b5ca8a0bf9
ppppangu <14707891+meidici911@user.noreply.gitee.com>
2025-06-16 12:52:08 +0800

    update

--- mineru_process.py
+++ mineru_process.py
@@ -927,9 +927,8 @@ async def mineru_process(file_url: str, knowledge_base_id: str, mode: str, user_
             database=pg_config["database"]
         )
             async with conn.transaction():
-                await conn.execute(f"UPDATE chunk_schema.documents SET markdown_public_url = '{md_file_public_url}' WHERE id = '{file_uuid}'")
-                await conn.execute(f"UPDATE chunk_schema.documents SET raw_file_public_url = '{pdf_file_public_url}' WHERE id = '{file_uuid}'")
-            logger.info(f"用户{user_id}上传md文件到minio成功: {md_file_public_url},且将源文件的pdf格式上传到minio成功: {pdf_file_public_url}")
+                await conn.execute(f"""UPDATE chunk_schema.documents SET markdown_public_url = '{md_file_public_url}', raw_file_public_url = '{pdf_file_public_url}', upload_time = now() WHERE id = '{file_uuid}'""")
+            logger.info(f"用户{user_id}上传md文件到minio成功: {md_file_public_url},且将源文件的pdf格式上传到minio成功: {pdf_file_public_url},且更新数据库成功")
 
             # 返回md文件的公网url
             return {"markdown_public_url": f"{config['server_components']['minio']['public_url_prefix']}/{bucket_name}/{user_id}/knowledgebase/{knowledge_base_id}/{file_uuid}/{file_uuid}.md",
