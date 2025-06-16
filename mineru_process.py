4f9881ebf4388008e5f72be97e99047315caf9b0
ppppangu <14707891+meidici911@user.noreply.gitee.com>
2025-06-16 10:53:19 +0800

    update

--- mineru_process.py
+++ mineru_process.py
@@ -927,7 +927,7 @@ async def mineru_process(file_url: str, knowledge_base_id: str, mode: str, user_
             database=pg_config["database"]
         )
             async with conn.transaction():
-                await conn.execute(f"UPDATE chunk_schema.documents SET md_file_public_url = '{md_file_public_url}' WHERE id = '{file_uuid}'")
+                await conn.execute(f"UPDATE chunk_schema.documents SET markdown_public_url = '{md_file_public_url}' WHERE id = '{file_uuid}'")
                 await conn.execute(f"UPDATE chunk_schema.documents SET raw_file_public_url = '{pdf_file_public_url}' WHERE id = '{file_uuid}'")
             logger.info(f"用户{user_id}上传md文件到minio成功: {md_file_public_url},且将源文件的pdf格式上传到minio成功: {pdf_file_public_url}")
 
